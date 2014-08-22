59180cd741db1de37b1e012decda52577cc03ff7
deuanz <deuanz.cpenu@gmail.com>
2014-08-22 23:30:02 +0700

    เพิ่มไฟล์ connect.php เพื่อไว้สำหรับเชื่อมต่อกับ DB :snail:

---
+++ AccidentReportServer/src/connect.php
@@ -0,0 +1,12 @@
+<?php
+	$host = "fdb7.runhosting.com";
+	$user = "1679495_dbacc";
+	$pass = "tot_1288";
+	$name = "1679495_dbacc";
+	 
+	$con = mysqli_connect($host, $user, $pass, $name);
+	
+	if (mysqli_connect_error()) {
+		echo "Fail to connect to mysql: " . mysqli_connect_error();
+	}
+?>
--- AccidentReportServer/src/saveToDB.php
+++ AccidentReportServer/src/saveToDB.php
@@ -1,13 +1,7 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 require_once('AccidentReport.php');
-$host="fdb7.runhosting.com";
-$username="1679495_dbacc";
-$pass_word="tot_1288";
-$DB="1679495_dbacc";
-
-// connect database
-$con=mysqli_connect( $host,$username,$pass_word,$DB) or die ("ติดต่อ Mysql ไม่ได้ ");
+include_once 'connect.php';
 $con->set_charset("utf8");
 
 //receive variavles from reportAccident.php file
