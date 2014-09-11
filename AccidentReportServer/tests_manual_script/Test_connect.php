ab7dc708168319f8595af6e3a60c8613402ab088
watcharakit <gun_1288@hotmail.com>
2014-09-11 22:36:06 +0700

    edit Test_connect.php

--- AccidentReportServer/tests_manual_script/Test_connect.php
+++ AccidentReportServer/tests_manual_script/Test_connect.php
@@ -1,20 +1,9 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 require_once('DB.php');
-function callConnect() {
-		$conn = new DB();
-        return $conn->connect();
-}
 
-function callClose(){
-		$conn = new DB();
-		return $conn->closeDB();
-}
-
-$con = callConnect();
-echo " Test Connection (Empty means No Error) = ".mysqli_error($con);
-//mysqli_close($con) or die("Can't Close Connection");
-echo '<br/>';
-echo "close connect (Empty means No Error) = ".callClose();
-
+$conn = new DB();
+$conn->connect();
+echo " Test Connection (Empty means No Error) = ".mysqli_error($conn->con);
+$conn->closeDB();
 ?>
